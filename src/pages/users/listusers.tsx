87ec4ecae243d7dfc2de06e19295166c03e96bdd
Sebasprzd <seperazade@est.utn.ac.cr>
2024-06-21 00:39:03 -0600

    first commit

---
+++ src/pages/users/listusers.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GetUsers from "../../components/users/getUsers";
+
+const ObtenerUsuarios = () => {
+    return (
+        <div className="container">
+            <h3>Uso de un componente externo</h3>
+    <br/>
+    <GetUsers />
+    </div>
+);
+}
+
+export default ObtenerUsuarios;
